2da3bd1bd5b449d8ceb3b7f6d54c972399ec3bc9
charlesDouc <charles.douc@gmail.com>
2018-02-13 14:15:53 -0500

    Original project

---
+++ Pastiche Game/Assets/Scripts/MainManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainManager : MonoBehaviour {
+
+    public int Timeleft = 10;
+
+
+    public int TotalScore;
+
+    private void Update()
+    {
+        GameObject.Find("ScoreLabel").GetComponent<Text>().text = "Score: " + TotalScore;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        StartCoroutine (CountDown());
+		
+	}
+	
+
+    IEnumerator CountDown()
+    {
+        while(Timeleft > 0)
+        {
+            yield return new WaitForSeconds(1);
+            Timeleft--;
+           
+
+        }
+    }
+
+}
