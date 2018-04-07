368d6e5ea8ad9fca78952ef322e99b9bc1de0783
charlesDouc <charles.douc@gmail.com>
2018-04-07 17:02:20 -0400

    Enhance the console (style).

---
+++ Pastiche Game/Assets/Scripts/Console/inputFieldController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputFieldController : MonoBehaviour {
+
+	// public variables
+
+	// private variables
+	private InputField m_input;
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		// Get the input field of the object
+		m_input = gameObject.GetComponent<InputField>();
+	}
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		// Always keep it activated
+		m_input.ActivateInputField();
+	}
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+
+}
